4272f67e6e42766511decd0760af19081f3dca7c
amzotti <azotti1337@gmail.com>
2015-02-15 17:27:56 -0800

    Add transition animation to all characters

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var movePlayer = _.throttle((function (event) {
       moveCharacter('left', '#player', _.add);
       break;
   }
-}), 250);
+}), 600);
 
 window.addEventListener("keydown", function (event) {
   movePlayer(event);
@@ -25,26 +25,17 @@ function moveCharacter(dir, character, fn) {
   var coord = d3.select(character).style(dir);
   coord = _.stripPixel(coord);
   coord = fn(+coord, 200) + 'px'; 
-  d3.select(character).transition().style(dir, coord);
+  d3.select(character).style(dir, coord);
 }
 
 function moveBots() {
   var bots = d3.selectAll('.bots')[0];
-  var dir = _.sample(['top', 'left']);
-  var fn = _.sample([_.add, _.subtract]);
   for (var i = 0;i < bots.length;i++) {
-    if (bots[i] !== undefined) {
-       var bot = d3.select(bots[i]);
-       var coord = bot.style(dir);
-       coord = _.stripPixel(coord);
-       coord = fn(+coord, 200) + 'px'; 
-       bot.style(dir, coord);
-       //Need to manually add CSS transitions. D3 transitions producing error
-    }
-  }
+    var dir = _.sample(['top', 'left']);
+    var fn = _.sample([_.add, _.subtract]);
+    moveCharacter(dir, bots[i], fn);
+   }
 }
-
-setInterval(moveBots, 3000);
 
 function rand(start, end) {
   if (end === undefined) {
@@ -54,7 +45,6 @@ function rand(start, end) {
   return Math.floor(Math.random() * (end - start)) + start;
 }
 
-
 d3.select('main')
   .selectAll('.bots')
   .data(d3.range(15))
@@ -62,4 +52,6 @@ d3.select('main')
   .append('div')
   .style('top', function() { return rand(100, window.innerHeight)})
   .style('left', function() { return rand(100, window.innerWidth)})
-  .attr('class', 'bots')
+  .attr('class', 'bots');
+
+setInterval(moveBots, 900);
